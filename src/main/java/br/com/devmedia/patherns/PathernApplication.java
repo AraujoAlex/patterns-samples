24b1be933b28162d89505539b7bb8f5923285b78
Alex Gonçalves de Araujo <alex.araujo@totvs.com.br>
2020-06-18 08:42:02 -0300

    Mudando os nomes das classes principais

--- src/main/java/br/com/devmedia/patherns/PathernApplication.java
+++ src/main/java/br/com/devmedia/patherns/PathernApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class PathernApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+		SpringApplication.run(PathernApplication.class, args);
 	}
 
 }
